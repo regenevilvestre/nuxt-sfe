77b0456c8b734b89046162753f173aa246a5d380
Regene Vilvestre <regene_vilvestre@ligph.com>
2021-01-25 16:28:45 +0800

    middleware redirects

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,5 +1,9 @@
-export default function ({ store, redirect }) {
-  if (!store.state.isAuth) {
+export default function ({ store, redirect, route }) {
+  if (route.name === '/new' && !store.state.isAuth) {
+    return redirect('/')
+  } else if (route.name === 'edit-id' && !store.state.isAuth) {
+    return redirect(`/view/${parseInt(route.params.id)}`)
+  } else if (!store.state.isAuth) {
     return redirect('/')
   }
 }
